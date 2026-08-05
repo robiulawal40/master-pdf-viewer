6974d80ceac4d6801642ed333428fb1ac7209faa
robiulawal40 <ral.sust@hotmail.com>
2026-08-05 22:50:30 +0600

    Use build_dir and assets_dir for WordPress.org deployment

--- prepare-dist.js
+++ prepare-dist.js
@@ -5,7 +5,6 @@ const ROOT = path.resolve(__dirname);
 const DIST = path.join(ROOT, 'dist');
 const WP_ORG = path.join(ROOT, '.wordpress-org');
 
-// Clean dist
 if (fs.existsSync(DIST)) {
   fs.rmSync(DIST, { recursive: true });
 }
@@ -23,13 +22,10 @@ const dirsToCopy = [
   'pdf-js',
 ];
 
-const copiedFiles = new Set();
-
 for (const file of filesToCopy) {
   const src = path.join(ROOT, file);
   if (fs.existsSync(src)) {
     fs.copyFileSync(src, path.join(DIST, file));
-    copiedFiles.add(file);
     console.log('Copied:', file);
   }
 }
@@ -38,35 +34,22 @@ for (const dir of dirsToCopy) {
   const src = path.join(ROOT, dir);
   if (fs.existsSync(src)) {
     copyDirSync(src, path.join(DIST, dir));
-    copiedFiles.add(dir);
     console.log('Copied dir:', dir);
   }
 }
 
-// Copy WordPress.org assets from .wordpress-org/ to dist root
 if (fs.existsSync(WP_ORG) && fs.statSync(WP_ORG).isDirectory()) {
   const entries = fs.readdirSync(WP_ORG, { withFileTypes: true });
   for (const entry of entries) {
-    const srcPath = path.join(WP_ORG, entry.name);
-    const destPath = path.join(DIST, entry.name);
     if (entry.isFile()) {
+      const srcPath = path.join(WP_ORG, entry.name);
+      const destPath = path.join(DIST, entry.name);
       fs.copyFileSync(srcPath, destPath);
-      copiedFiles.add(entry.name);
       console.log('Copied asset:', entry.name);
     }
   }
 }
 
-// Write .distignore inside dist/ so deploy only includes copied files
-const distignore = ['*'];
-for (const f of copiedFiles) {
-  if (f.endsWith('/')) {
-    distignore.push('!' + f);
-  } else {
-    distignore.push('!' + f);
-  }
-}
-fs.writeFileSync(path.join(DIST, '.distignore'), distignore.join('\n') + '\n');
 console.log('dist/ ready for deployment.');
 
 function copyDirSync(src, dest) {
