b5d4b828865722d7d607f2b1ded1221e40c0f27b
robiulawal40 <ral.sust@hotmail.com>
2026-08-07 16:07:16 +0600

    Update workflow and rebuild build artifacts

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '92c20b3979a77ab94711');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '61be8d99fa0e6259c29e');
